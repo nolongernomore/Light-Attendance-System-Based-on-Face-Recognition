24c11fbc81251c4352289343f05ed0aad596ac3f
left7-big <2806938867@qq.com>
2026-09-06 00:16:02 +0800

    Revert "My frontend"

--- frontend/src/api/request.js
+++ frontend/src/api/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'http://26.6.49.51:8000',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000',
   timeout: 15000
 })
 
